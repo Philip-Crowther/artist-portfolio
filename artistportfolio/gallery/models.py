780d9854579c162e65dd5b3d9206d34015b642e2
Philip Crowther <crowtherpb@gmail.com>
2022-01-23 15:00:41 -0500

    debug

--- artistportfolio/gallery/models.py
+++ artistportfolio/gallery/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 class Gallery(models.Model):
     """Model representing a gallery of images"""
-    project_title = models.CharField()
+    gallery_name = models.CharField(max_length=40)
 
     def __str__(self):
         """string for representing the Model object"""
-        return self.project_title
+        return self.gallery_name
 
         
 class Image(models.Model):
     """Model representing a particular image/work of art"""
-    photo = models.ImageField()
-    title = models.CharField
+    image = models.ImageField()
+    title = models.CharField(max_length=30)
     gallery = models.ForeignKey('Gallery', on_delete=models.CASCADE)
 
     def __str__(self):
