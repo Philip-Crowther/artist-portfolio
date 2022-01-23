251bb0ef84742b2b9d97eefd5ddf145f55b42be4
Philip Crowther <crowtherpb@gmail.com>
2022-01-23 14:03:45 -0500

    register models

--- artistportfolio/gallery/admin.py
+++ artistportfolio/gallery/admin.py
@@ -1,3 +1,7 @@
+from re import I
 from django.contrib import admin
+from .models import Gallery, Image
 
 # Register your models here.
+admin.site.register(Gallery)
+admin.site.register(Image)
--- artistportfolio/gallery/views.py
+++ artistportfolio/gallery/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from gallery.models import Gallery, Image
 
 # Create your views here.
+def gallery(request, gallery_name):
+    """View function for gallery of site"""
+    # load all images in gallery as context
+    gallery = Gallery.objects.get(gallery_name=gallery_name)
+
+    context = {
+        images: gallery.image_set.all(),
+    }
+    return 
