4a037971983fb8507cff3889ae8da5acdaec21bd
Philip Crowther <crowtherpb@gmail.com>
2022-03-06 15:38:49 -0500

    pre-static fix

--- artistportfolio/artistportfolio/settings.py
+++ artistportfolio/artistportfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,8 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+# Path where media is stored
+# Note: for some reason this breaks galleries
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
